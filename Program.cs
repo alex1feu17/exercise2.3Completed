548897739e08d601118d15ddf259e5ca57b1785e
alex1feu17 <alexyd@hotmail.ca>
2020-01-28 20:27:18 -0500

    exercise2.3 Complete

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace exercise2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string premiernombre = "";
+            string deuxiemenombre = "";
+            string symbole = "";
+            int total = 0;
+
+            Console.WriteLine("Quelle est votre premier nombre");
+            premiernombre= Console.ReadLine();
+            int nombre1 = Int32.Parse(premiernombre);
+
+            Console.WriteLine("Quelle est votre deuxieme nombre");
+            deuxiemenombre = Console.ReadLine();
+            int nombre2 = Int32.Parse(deuxiemenombre);
+
+            Console.WriteLine("Quelle est votre symbole d'equation ");
+            symbole = Console.ReadLine();
+
+            if(symbole=="+")
+            {
+                total = nombre1 + nombre2;
+            }
+            if (symbole == "-")
+            {
+                total = nombre1 - nombre2;
+            }
+            if (symbole == "*")
+            {
+                total = nombre1 * nombre2;
+            }
+            if (symbole == "/")
+            {
+                total = nombre1 / nombre2;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
